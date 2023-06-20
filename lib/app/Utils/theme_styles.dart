a3b2821dbed6c4907e4e69234a312642242be212
ScribbleRiyad <scribbleriyad@gmail.com>
2023-06-20 15:59:10 +0600

    Utils and Widgets Sets

---
+++ lib/app/Utils/theme_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  // static const Color scaffoldBackgroundColor = Color(0xFFDAEFFF);
+  // static const Color primaryColor = Color(0xFF000096);
+  // static const Color primaryAccentColor = Color(0xFF73D5FF);
+
+  //Flutter Color
+  static const Color primaryTextColor = Colors.white;
+  static const Color secondaryTextColor = Colors.black;
+  static const Color formBackgroundColor = Color(0xFFD5E3F9);
+  static const Color disabledColor = Colors.black38;
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
+  static const Color redColor = Colors.red;
+  static const Color greenColor = Colors.green;
+  static const Color redColorAccent = Color(0xFFFFA7A7);
+
+  /// font Size
+  static const double fontSize = 16.0;
+}
