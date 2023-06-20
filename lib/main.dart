73d729bb673d421a4bff272a72fc12f3a8ba2c32
ScribbleRiyad <scribbleriyad@gmail.com>
2023-06-20 16:32:30 +0600

    main file Setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+import 'package:responsive_framework/utils/scroll_behavior.dart';
 import 'app/Route/route_importer.dart';
+import 'app/Utils/app_translations.dart';
 
 void main() {
   runApp(DemoApp());
@@ -13,13 +17,30 @@ class DemoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _appRouter.config(),
+    return GetMaterialApp.router(
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      builder: (context, child) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, child!),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(450, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
       ),
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      translations: AppTranslations(),
+      locale: Get.locale,
+      fallbackLocale: const Locale('bn', 'BD'),
     );
   }
 }
